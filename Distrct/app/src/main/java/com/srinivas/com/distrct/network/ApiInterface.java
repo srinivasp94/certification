2c796ae023deb47fe09d228bcd6e0195dbcb8eb5
unknown <srinivas.pudi94@gmail.com>
2018-06-03 21:38:48 +0530

    casual commit

---
+++ Distrct/app/src/main/java/com/srinivas/com/distrct/network/ApiInterface.java
@@ -0,0 +1,9 @@
+package com.srinivas.com.distrct.network;
+
+/**
+ * Created by rk on 03-Jun-18.
+ */
+
+public interface ApiInterface {
+
+}
---
+++ Distrct/app/src/main/java/com/srinivas/com/distrct/network/RetrofitApiClient.java
@@ -0,0 +1,32 @@
+package com.srinivas.com.distrct.network;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by rk on 03-Jun-18.
+ */
+
+public class RetrofitApiClient {
+    public static final String BASE_URL = "";
+    private static Retrofit retrofit = null;
+
+
+    public static Retrofit getClient() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+        return retrofit;
+    }
+}
